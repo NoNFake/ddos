4865f6d635f39d35606e4b9554f338fdab2c5876
yurii <uraklimek@gmail.com>
2025-10-22 22:21:12 +0300

    some updates

--- ddos/__main__.py
+++ ddos/__main__.py
@@ -1,5 +1,5 @@
 from . import ddos
-
+import os
 
 preview = """
     -t <target_url> -tr <thread_count> -n <sleep_time>
@@ -7,9 +7,37 @@ preview = """
 
 """
 
+menu = f"""
+
+  - http -
+[t] target_url: 
+[n] sleep_time:
+[tr] thread_count:
+
+
+  - udp/tcp -
+
+
+[u] update
+
+"""
+
+
+
+def main():
+    print(menu)
+
+    choose = input("> ")
+
+    if choose == "t":
+        target_url = input("target_url: ")
+        print(f"Target URL set to: {target_url}")
+    if choose == "u":
+        # ddos.update_tool()
+        os.system("git pull && uv run -m ddos")
 
 
 
 if __name__ == "__main__":
-    print(preview)
-    ddos.main()   
+    # ddos.main()   
+    main()
